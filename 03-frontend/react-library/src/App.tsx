bf94da4820447988e5b5c0e8d328693a194bfbd3
Jenith Rajlawat <jenithrajlawat@gmail.com>
2023-10-23 12:59:43 +0545

    Added Heros.tsx

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { NavBar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
 import { Carousel } from './layouts/HomePage/Carousel';
+import { Heros } from './layouts/HomePage/Heros';
 function App() {
   return (
     <div>
       <NavBar />
       <ExploreTopBooks />
       <Carousel />
+      <Heros />
     </div>
   );
 }
